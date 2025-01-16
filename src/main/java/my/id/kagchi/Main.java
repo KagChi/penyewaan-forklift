1ba9052a1e3fc4a37e3e84083e151e409a6a26d7
KagChi <yonuxbot@gmail.com>
2025-01-16 15:53:34 +0700

    chore: update readme

--- src/main/java/my/id/kagchi/Main.java
+++ src/main/java/my/id/kagchi/Main.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 
 public class Main {
     private final static String title = "Penyewaan Forklift";
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             LoginForm loginForm = new LoginForm(title);
